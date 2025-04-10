373ebfea429f1f9f59156da531a79b5d9e48289a
rileyk94 <rileyk94@gmail.com>
2025-04-10 17:40:56 -0500

    last commit was in wrong folder

--- compare.js
+++ compare.js
@@ -4,8 +4,7 @@ const shirtBase = document.getElementById("shirtBase");
 const designOverlay = document.getElementById("designOverlay");
 const buttons = document.querySelectorAll(".shirt-button");
 const shirtLabel = document.getElementById("shirtLabel");
-const backimages = ["Cartoon-pride.png","Cartoon-Red-Pins.png","cartoon-stars.png","Old-Map-Flags.png","Old-Map-Pins.png","pride-map-pins.png"]
-const frontimages = ["Steampunk-round-badge.png","30-green-badge.png","Drawn-round-badge.png","line-round-badge.png","lines-badge.png","lines-badge.png","lines-badge.png","wreath-badge.png","Steampunk-round-large.png","30-green-large.png","Drawn-round-large.png","line-round-large.png","lines-large.png","stay-salty-large.png","wreath-large.png","cartoon-front-anniversary.png","cartoon-front-overlay.png","old-flag-front.png","old-pin-front.png"]
+const leftImages = ["Steampunk-round-badge.png","30-green-badge.png","Drawn-round-badge.png","line-round-badge.png","lines-badge.png","lines-badge.png","lines-badge.png","wreath-badge.png","Steampunk-round-large.png","30-green-large.png","Drawn-round-large.png","line-round-large.png","lines-large.png","stay-salty-large.png","wreath-large.png","cartoon-front-anniversary.png","cartoon-front-overlay.png","old-flag-front.png","old-pin-front.png","Cartoon-pride.png","Cartoon-Red-Pins.png","cartoon-stars.png","Old-Map-Flags.png","Old-Map-Pins.png","pride-map-pins.png"]
 
 designSelect.addEventListener("change", () => {
     designOverlay.src = "./images/back-designs/" + designSelect.value;
@@ -37,6 +36,8 @@ buttonsFL.forEach(button => {
         shirtLabelFL.textContent = label;
     })
 })
+
+
 // Right side Back Script
 // -------------------------------------------
 const designSelectRight = document.getElementById("designSelectRight");
@@ -44,6 +45,7 @@ const shirtBaseRight = document.getElementById("shirtBaseRight");
 const designOverlayRight = document.getElementById("designOverlayRight");
 const buttonsRight = document.querySelectorAll(".shirt-buttonR");
 const shirtLabelRight = document.getElementById("shirtLabelRight");
+const rightImages = ["Steampunk-round-badge.png","30-green-badge.png","Drawn-round-badge.png","line-round-badge.png","lines-badge.png","lines-badge.png","lines-badge.png","wreath-badge.png","Steampunk-round-large.png","30-green-large.png","Drawn-round-large.png","line-round-large.png","lines-large.png","stay-salty-large.png","wreath-large.png","cartoon-front-anniversary.png","cartoon-front-overlay.png","old-flag-front.png","old-pin-front.png","Cartoon-pride.png","Cartoon-Red-Pins.png","cartoon-stars.png","Old-Map-Flags.png","Old-Map-Pins.png","pride-map-pins.png"]
 
 designSelectRight.addEventListener("change", () => {
     designOverlayRight.src = "./images/back-designs/" + designSelectRight.value;
@@ -77,16 +79,5 @@ buttonsFR.forEach(button => {
     })
 })
 
-//Compar Front Left Functions
-designSelectFL.addEventListener("change", () => {
-    designOverlay.src = "./images/front-designs/" + designSelectFL.value;
-})
 
-buttonsFL.forEach(button => {
-    button.addEventListener("click", () => {
-        const shirtFile = button.getAttribute("data-shirt");
-        const label = button.getAttribute("data-label");
-        shirtBase.src = "./images/shirt-fronts/" + shirtFile;
-        shirtLabelFL.textContent = label;
-    })
-})
+
