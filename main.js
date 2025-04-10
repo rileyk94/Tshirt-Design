8fd223a42b378bb25d7f4244c9be7843c2c54945
rileyk94 <rileyk94@gmail.com>
2025-04-10 16:59:49 -0500

    chore: add front images array

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const shirtBase = document.getElementById("shirtBase");
 const designOverlay = document.getElementById("designOverlay");
 const buttons = document.querySelectorAll(".shirt-button");
 const shirtLabel = document.getElementById("shirtLabel");
-
+const frontimages = ["Steampunk-round-badge.png","30-green-badge.png","Drawn-round-badge.png","line-round-badge.png","lines-badge.png","lines-badge.png","lines-badge.png","wreath-badge.png","Steampunk-round-large.png","30-green-large.png","Drawn-round-large.png","line-round-large.png","lines-large.png","stay-salty-large.png","wreath-large.png","cartoon-front-anniversary.png","cartoon-front-overlay.png","old-flag-front.png","old-pin-front.png"]
 //Main Button Functions
 designSelect.addEventListener("change", () => {
     designOverlay.src = "./images/front-designs/" + designSelect.value;
