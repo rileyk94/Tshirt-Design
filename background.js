ef27feb586ae394ffce2a17196fc1d35ad6e0a85
rileyk94 <rileyk94@gmail.com>
2025-04-11 15:21:17 -0500

    feat: add color button and dropdown functionality

--- background.js
+++ background.js
@@ -1,4 +1,18 @@
 const designSelect = document.getElementById("designSelect");
 const colorBase = document.getElementById("colorBase");
+const design = document.getElementById("design");
 const buttons = document.querySelectorAll(".shirt-button");
-const shirtLabel = document.getElementById("shirtLabel")
+const shirtLabel = document.getElementById("shirtLabel");
+const images = [];
+
+designSelect.addEventListener("change", () => {
+    design.src = "./images/all-designs/"+ designSelect.value;
+})
+
+buttons.forEach (button => {
+    button.addEventListener("click", () => {
+        const shirtFile = button.getAttribute("data-label");
+        colorBase.src = "./images/color-backgrounds/" + shirtFile + ".png";
+        shirtLabel.textContent = shirtFile;
+    })
+})
