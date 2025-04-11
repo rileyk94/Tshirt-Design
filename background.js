ed8c3f82dfe1a098190afb17d3119d353d6d885e
rileyk94 <rileyk94@gmail.com>
2025-04-11 14:33:33 -0500

    feat: add background colors to backgrounds page

---
+++ background.js
@@ -0,0 +1,4 @@
+const designSelect = document.getElementById("designSelect");
+const colorBase = document.getElementById("colorBase");
+const buttons = document.querySelectorAll(".shirt-button");
+const shirtLabel = document.getElementById("shirtLabel")
