789e7acf64a62b779caeefb6fd31ec4b713ce0dc
rileyk94 <rileyk94@gmail.com>
2025-04-11 15:44:48 -0500

    feat: add design label

--- background.js
+++ background.js
@@ -3,10 +3,16 @@ const colorBase = document.getElementById("colorBase");
 const design = document.getElementById("design");
 const buttons = document.querySelectorAll(".shirt-button");
 const shirtLabel = document.getElementById("shirtLabel");
-const images = [];
+const designLabel = document.getElementById("designLabel")
+const images = ["pride.png","30th-green.png","cartoon-30th.png","cartoon-front.png","cartoon-markers.png","cartoon-pride-1.png",
+    "cartoon-pride-2.png","cartoon-pride-3.png","cartoon-pins.png","cartoon-yellow-stars.png","drawn-compass.png","lines-30.png",
+    "old.png","old-flags-back.png","old-pins-back.png","pride-badge.png","stay-salty.png","steampunk.png","vector-compass.png",
+    "Wreath-light.png","vector-compass-light.png","steampunk-light.png","stay-salty-light.png","old-light.png","vector-30th-light.png",
+    "drawn-compass-light.png","cartoon-light.png","cartoon-30th-light.png","30th-green-light.png","",];
 
 designSelect.addEventListener("change", () => {
     design.src = "./images/all-designs/"+ designSelect.value;
+    designLabel.textContent = designSelect.options[designSelect.selectedIndex].text
 })
 
 buttons.forEach (button => {
