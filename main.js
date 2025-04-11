e2166332579fea4ea29fde147430bf509d3f1cd6
rileyk94 <rileyk94@gmail.com>
2025-04-10 21:24:49 -0500

    fix: update arrowkey and cycle button functions to match new toggle feature

--- main.js
+++ main.js
@@ -4,15 +4,16 @@ const shirtBase = document.getElementById("shirtBase");
 const designOverlay = document.getElementById("designOverlay");
 const buttons = document.querySelectorAll(".shirt-button");
 const shirtLabel = document.getElementById("shirtLabel");
-const frontImages = ["Steampunk-round-badge.png","30-green-badge.png","Drawn-round-badge.png","line-round-badge.png","lines-badge.png","lines-badge.png","lines-badge.png","wreath-badge.png","Steampunk-round-large.png","30-green-large.png","Drawn-round-large.png","line-round-large.png","lines-large.png","stay-salty-large.png","wreath-large.png","cartoon-front-anniversary.png","cartoon-front-overlay.png","old-flag-front.png","old-pin-front.png"]
+const frontImages = ["Steampunk-round-badge.png","30-green-badge.png","Drawn-round-badge.png","line-round-badge.png","lines-badge.png","pride-badge.png","stay-salty-badge.png","wreath-badge.png","Steampunk-round-large.png","30-green-large.png","Drawn-round-large.png","line-round-large.png","lines-large.png","stay-salty-large.png","wreath-large.png","cartoon-front-anniversary.png","cartoon-front-overlay.png","old-flag-front.png","old-pin-front.png"]
+const backImages = ["Cartoon-pride.png","Cartoon-Red-Pins.png","cartoon-stars.png","Old-Map-Flags.png","Old-Map-Pins.png","pride-map-pins.png"]
 
 //Main Button Functions
 let isFront = true;
 let currentColor = "White";
-let currentDesign = isFront ? "30-green-badge" : "Cartoon-pride";
+let currentDesign = isFront ? "Steampunk-round-badge" : "Cartoon-pride";
 
 const toggleBtn = document.getElementById("toggleView")
-
+updateDesignSelectorVisibility();
 toggleBtn.addEventListener("click", () => {
     isFront = !isFront;
     updateShirtImage();
@@ -59,6 +60,7 @@ designSelectBack.addEventListener("change", () => {
     currentDesign = designSelectBack.value;
     if (!isFront) updateDesignImage();
 })
+// old code:--------------------------------------------------------------
 // designSelect.addEventListener("change", () => {
 //     designOverlay.src = "./images/front-designs/" + designSelect.value;
 // })
@@ -71,19 +73,21 @@ designSelectBack.addEventListener("change", () => {
 //         shirtLabel.textContent = label;
 //     })
 // })
-//cycle button functions
+//-------------------------------------------------------------------------
+//
+// cycle button functions:
 const previousButton = document.getElementById("previous");
 const nextButton = document.getElementById("next");
 
 let currentIndex = 0;
-const lastIndex = frontImages.length - 1;
+const lastIndex = isFront ? (frontImages.length - 1) : (backImages.length-1);
 
 previousButton.addEventListener("click", function() {
     if (currentIndex === 0) {
-        currentIndex = frontImages.length
+        currentIndex = isFront ? frontImages.length : backImages.length;
     }
     currentIndex = currentIndex-1
-    designOverlay.src = "./images/front-designs/" + frontImages[currentIndex]
+    designOverlay.src = `./images/${isFront ? 'front-designs/' : 'back-designs/'}${isFront ? frontImages[currentIndex] : backImages[currentIndex]}`;
 })
 
 nextButton.addEventListener("click", function() {
@@ -91,23 +95,23 @@ nextButton.addEventListener("click", function() {
         currentIndex = 0
     }
     currentIndex = currentIndex+1
-    designOverlay.src = "./images/front-designs/" + frontImages[currentIndex]
+    designOverlay.src = `./images/${isFront ? 'front-designs/' : 'back-designs/'}${isFront ? frontImages[currentIndex] : backImages[currentIndex]}`
 })
 
 document.addEventListener('keydown', (event) => {
     if (event.key === 'ArrowRight') {
         if (currentIndex === lastIndex) {
-            currentIndex = 0
+            currentIndex = -1
         }
         currentIndex = currentIndex+1
-        designOverlay.src = "./images/front-designs/" + frontImages[currentIndex]
+        designOverlay.src = `./images/${isFront ? 'front-designs/' : 'back-designs/'}${isFront ? frontImages[currentIndex] : backImages[currentIndex]}`;
     }
     else if (event.key == 'ArrowLeft') {
         if (currentIndex === 0) {
             currentIndex = frontImages.length
         }
         currentIndex = currentIndex-1
-        designOverlay.src = "./images/front-designs/" + frontImages[currentIndex]
+        designOverlay.src = `./images/${isFront ? 'front-designs/' : 'back-designs/'}${isFront ? frontImages[currentIndex] : backImages[currentIndex]}`
     }
 })
 
